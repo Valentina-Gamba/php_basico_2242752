6a78b85b99f2bf66a89b7b505ea3f6b815ed1c03
Valentina-Gamba <83036530+Valentina-Gamba@users.noreply.github.com>
2021-04-27 09:11:47 -0500

    proyecto creado

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('variables' , function(){
+    $mensaje = 20;
+    //Funcion nativa de PHP: analisa el contenido de la variable
+    //Variable: tipo de dato, datos
+    var_dump($mensaje);
+    echo "<hr />";
+    $mensaje = "Hola 2242753";
+    var_dump($mensaje);
+} );
+
+Route::get('arreglos' , function(){
+    //Definir un arreglo en PHP
+    //Tamaño: numero de elementos del arreglo
+    //Tamaño del arreglo estudiantes es 3
+    $estudiantes = [ "AN" => "Ana" ,
+                     2 => "Valeria" ,
+                     "JO" => "Jorge" ];
+    echo "<pre>";
+    print_r($estudiantes);
+    echo "</pre>";
+} );
